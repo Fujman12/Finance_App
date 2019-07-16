53b6432ba289be0913ff098b3f731a411a33e239
Armen Martikian <aremnbizz@gmail.com>
2019-07-16 12:01:37 +0300

    Updated save model UI Fixed bug with admin-panel
    Signed-off-by: Armen Martikian <aremnbizz@gmail.com>

--- app/admin.py
+++ app/admin.py
@@ -3,5 +3,7 @@ from .models import inputDB
 
 @admin.register(inputDB)
 class AuthorAdmin(admin.ModelAdmin):
-    fields = ('id', 'title', 'date_created', 'date_modified', 'user')
+    fields = ('id', 'title', 'date_created', 'date_modified', 'user', 'data')
     list_display = ('id', 'title', 'date_created', 'date_modified', 'user')
+
+    readonly_fields = ('id', 'data')
